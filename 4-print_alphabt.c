e7900ccef4a85c2ebe25a6e4c2acbdd2a38b6e00
ilias <benhammanilias@gmail.com>
2023-03-19 18:50:16 -0700

    C FOR DUMP

---
+++ 4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (letter != 'e' && letter != 'q')
+		putchar(c);
+	}
+	putchar('\n');
+		
+	return (0);
+}
---
+++ 5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	
+	int digit;
+
+	for (digit = '0'; c <= '9'; digit++)
+	{
+		printf("%i",digit);
+	}
+	putchar('\n');
+		
+	return (0);
+}
