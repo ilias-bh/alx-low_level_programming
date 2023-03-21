69ca83fc0dc0a5202a8884ee2999ae08c79e425c
ilias <benhammanilias@gmail.com>
2023-03-20 17:01:33 -0700

    C

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,9 +10,9 @@ int main(void)
 
 	int digit;
 
-	for (digit = '0'; digit <= '9'; digit++)
+	for (digit = 0; digit < 10; digit++)
 	{
-		printf("%i",digit);
+		printf("%i", digit);
 	}
 	putchar('\n');
 	return (0);
